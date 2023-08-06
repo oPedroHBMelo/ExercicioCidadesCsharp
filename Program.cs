83569471773673b6ce47ad270f2dcf1af41834dd
Pedro Henrique <pedrohbm554@gmail.com>
2023-08-06 02:58:31 -0300

    Inclusão dos nomes das cidades.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("O Litoral Sul compreende alguns municípios da Baixada Santista e outros tantos do Vale do Ribeira");
+Console.WriteLine("Cananéia");
+Console.WriteLine("Iguape");
+Console.WriteLine("Ilha Comprida");
+Console.WriteLine("Itanhaém");
+Console.WriteLine("Mongaguá");
+Console.WriteLine("Peruíbe");
+
+//bibliografia: https://pt.wikivoyage.org/wiki/Litoral_Sul_(São_Paulo)
